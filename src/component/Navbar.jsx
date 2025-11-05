418aa2a9ccdec46f49721e20d1489646c1418698
SylvesterFraide <fridaysylvester2@gmail.com>
2025-11-05 21:19:25 +0300

    4th commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState("Home");
+
   return (
-    <div className="">
+    <div className="text-gray-900 flex items-center justify-between py-4">
       <h2 className="text-2xl font-semibold">Logo</h2>
 
-      <div className="">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/collection">Collection</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+      <div className="flex gap-4">
+        <NavLink to="/" onClick={() => setActiveMenu("Home")}>
+          <p> Home</p>
+          <hr
+            className={activeMenu === "Home" ? "w-9 h-[1.5px] bg-gray-700" : "hidden"}
+          />
+        </NavLink>
+        <NavLink to="/collection" onClick={() => setActiveMenu("Collection")}>
+          <p>Collection</p>
+          <hr
+            className={
+              activeMenu === "Collection" ? "w-9 h-[1.5px] bg-gray-700" : "hidden"
+            }
+          />
+        </NavLink>
+        <NavLink to="/about" onClick={() => setActiveMenu("About")}>
+          <p>About</p>
+          <hr
+            className={
+              activeMenu === "About" ? "w-9 h-[1.5px] bg-gray-700" : "hidden"
+            }
+          />
+        </NavLink>
+        <NavLink to="/contact" onClick={() => setActiveMenu("Contact")}>
+          <p>Contact </p>
+          <hr
+            className={
+              activeMenu === "Contact" ? "w-9 h-[1.5px] bg-gray-700" : "hidden"
+            }
+          />
+        </NavLink>
       </div>
 
-      <div className="">
+      <div className="flex gap-4">
         <NavLink to="/search">
           <SearchIcon />
         </NavLink>
