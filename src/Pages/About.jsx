98da5def97ede986705354e75630ddb27ae43550
SylvesterFraide <fridaysylvester2@gmail.com>
2025-11-18 20:32:37 +0300

    14th commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,25 +4,29 @@ import Image2 from "../assets/Hero/Hero5.webp";
 
 const About = () => {
   return (
-    <div>
-      <div className="">
+    <div className="flex flex-col justify-center items-center my-10 gap-2 md:gap-6">
+      <div className="flex justify-center items-center mx-10 flex-col md:flex-row gap-10">
         <div className="">
-          <h1 className="font-bold uppercase text-2xl text-gray-600 ">About <span className="text-yellow-600">Us</span> </h1>
-          <p className="text-md md:text-lg text-gray">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis nisi
+          <h1 className="font-bold uppercase text-2xl text-gray-600 ">
+            About <span className="text-yellow-600">Us</span>{" "}
+          </h1>
+          <p className="text-gray-600 text-md md:text-lg w-3/4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Quis nisi
             cum, ab hic voluptas iure aperiam neque ex! Non quae aspernatur
             harum magni voluptatem. Nostrum amet tempora repellendus aspernatur
             ipsa.
           </p>
         </div>
-        <img src={Image2} alt="Image 2" className="" />
+        <img src={Image2} alt="Image 2" className="object-fit w-100 rounded-lg shadow-lg" />
       </div>
 
-      <div className="">
-        <div className="">
-          <img src={Image2} alt="Image 2" />
-        </div>
-        <div className="">
+      <div className="flex flex-col md:flex-row gap-10 my-10 justify-center h-1/2 items-center">
+        <img
+          src={Image1}
+          alt="Image 1"
+          className="object-fit w-120 rounded-lg shadow-lg"
+        />
+        <div className="flex flex-col justify-center items-center">
           <div className="">
             <h1>Our Mission</h1>
             <p>
--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import Title from "../component/Title";
 
 const Collection = () => {
   return (
-    <div>Collection</div>
-  )
-}
+    <div className="flex justify-between flex-col sm:flex-row gap-2 sm:gap-10 mt-6 mx-10 bg-gray-100 shadow-xl">
+      <div className="min-w-60">
+        <Title text2={"FILTERS"} />
+        <div className="border border-gray-300">
+          <div className="">
+            <h2 className="text-md font-semibold text-gray-700 p-2">
+              Categories
+            </h2>
+            <div className="flex flex-col gap-1 p-2">
+              <p className="flex gap-2 text-sm text-gray-600">
+                <input type="checkbox" value={"Men"} />
+                Men
+              </p>
+              <p className="flex gap-2 text-sm text-gray-600">
+                <input type="checkbox" value={"Women"} />
+                Women
+              </p>
+              <p className="flex gap-2 text-sm text-gray-600">
+                <input type="checkbox" value={"Kids"} />
+                Kids
+              </p>
+            </div>
+          </div>
 
-export default Collection
+          <div className="">
+            <h2 className="text-md font-semibold text-gray-700 p-2">
+              Type
+            </h2>
+            <div className="flex flex-col gap-1 p-2">
+              <p className="flex gap-2 text-sm text-gray-600">
+                <input type="checkbox" value={"TopWear"} />
+                TopWear
+              </p>
+              <p className="flex gap-2 text-sm text-gray-600">
+                <input type="checkbox" value={"BottomWear"} />
+                BottomWear
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border flex-1">
+        <Title text2={"PRODUCTS"} />
+      </div>
+    </div>
+  );
+};
+
+export default Collection;
